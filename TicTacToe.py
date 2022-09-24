4a75af88cc8bd9a4583a057361574cc3d5b6abb9
gem <airmooseandr@gmail.com>
2022-09-24 04:41:06 -0700

    TicTacToe test + breakdown

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,15 @@
-i = 0
-while i != 6969:
-    i+=1
-    print(i)
+import random
+
+class Game:
+    def __init__(self) -> None:
+        self.cells = []
+    def create_cells(self):
+        for i in range(3):
+            row = []
+            for c in range(3):
+                row.append('~')
+            self.cells.append(row)
+    def player_randomizer(self):
+        return random.randint(0,1)
+    def action(self, row, col, player):
+        self.board[row][col] = player
